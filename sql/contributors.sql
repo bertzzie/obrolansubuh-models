1f28df22b4730bb83c9ce265ca18b9f093114bb6
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-07-08 14:55:37 +0700

    Updated SQL

--- sql/contributors.sql
+++ sql/contributors.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS contributors (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password TEXT NOT NULL,
-    about TEXT DEFAULT NULL,
-    join_since DATETIME NOT NULL,
-    photo TEXT DEFAULT NULL,
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   TEXT         NOT NULL,
+    about      TEXT         DEFAULT NULL,
+    join_since TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    photo      TEXT         DEFAULT NULL,
     
     PRIMARY KEY (id)
 );
--- sql/posts.sql
+++ sql/posts.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS posts (
     id           BIGINT        NOT NULL AUTO_INCREMENT,
     title        VARCHAR(1024) NOT NULL,
     content      TEXT          NOT NULL,
-    author       BIGINT        NOT NULL,
+    author_id    BIGINT        NOT NULL,
     published    BOOLEAN       DEFAULT FALSE,
-    created      DATETIME      DEFAULT CURRENT_TIMESTAMP,
-    updated      DATETIME      DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    created      TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+    updated      TIMESTAMP     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     
     PRIMARY KEY (id),
     FOREIGN KEY (author) REFERENCES contributors(id)
