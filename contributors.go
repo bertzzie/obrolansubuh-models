013c44bc42ea961513f0572b7f670b5fb8dd7a1c
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-09-24 11:12:55 +0700

    contributors now have handle

--- contributors.go
+++ contributors.go
@@ -3,12 +3,15 @@ package models
 import (
 	"github.com/revel/revel"
 	"golang.org/x/crypto/bcrypt"
+	"regexp"
+	"strings"
 	"time"
 )
 
 type Contributor struct {
 	ID        int64
 	Name      string    `sql:"size:255;not null;index:name"`
+	Handle    string    `sql:"size:255;not null;index:handle"`
 	Email     string    `sql:"size:255;not null;unique_index"`
 	Password  string    `sql:"type:text;not null"`
 	About     string    `sql:"type:text;not null"`
@@ -23,6 +26,7 @@ type Contributor struct {
 
 func (c Contributor) Validate(v *revel.Validation) {
 	v.Required(c.Name)
+	v.Required(c.Handle)
 	v.Required(c.Email)
 	v.Required(c.Password)
 }
@@ -44,4 +48,18 @@ func (c *Contributor) SetPassword(password string) (string, error) {
 
 	c.Password = pass
 	return pass, nil
+}
+
+func (c *Contributor) NameSlug() string {
+	reg, err := regexp.Compile("[^A-Za-z0-9- ]+")
+	if err != nil {
+		revel.ERROR.Println("[LGERR] Failed to compile regex in Models.Contributors.NameSlug")
+		return ""
+	}
+
+	safe := reg.ReplaceAllString(c.Name, "")
+	safe = strings.Replace(safe, " ", "-", -1)
+	safe = strings.Trim(safe, " ")
+
+	return safe
 }
